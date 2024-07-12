17efd095a667d4c4d82ebc3fab3b51562dc8bcfc
hannael_abrams <hannaelabrams@gmail.com>
2024-07-12 11:27:16 +0200

    delete try-catch

--- server.ts
+++ server.ts
@@ -8,26 +8,17 @@ import checkConnection from "./src/config/checkConnectionDb";
 const app = express();
 const upload = multer();
 
-const startServer = async () => {
-  try {
-    await checkConnection();
+checkConnection();
 
-    app.use(bodyParser.json());
-    app.use(upload.none());
+app.use(bodyParser.json());
+app.use(upload.none());
 
-    app.use("/", personRoutes);
+app.use("/", personRoutes);
 
-    app
-      .listen(config.port, () => {
-        console.log("[Server API] Server running at PORT: ", config.port);
-      })
-      .on("[Server API] Server error", (error) => {
-        throw new Error(error.message);
-      });
-  } catch (error) {
-    console.error("[Gestion_Menage] Failed to start server:", error);
-    process.exit(1);
-  }
-};
-
-startServer();
+app
+  .listen(config.port, () => {
+    console.log("[Server API] Server running at PORT: ", config.port);
+  })
+  .on("[Server API] Server error", (error) => {
+    throw new Error(error.message);
+  });
