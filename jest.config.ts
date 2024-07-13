d6b3f059ff4c55ac895be5d338e8cc5e52d0982f
hannael_abrams <hannaelabrams@gmail.com>
2024-07-14 00:52:13 +0200

    Initialize test with Jest

---
+++ jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  collectCoverageFrom: ["src/**/*.ts"],
+  transform: {
+    "^.+\\.ts$": "ts-jest",
+  },
+};
+
+export default config;
--- src/config/checkConnectionDb.ts
+++ src/config/checkConnectionDb.ts
@@ -1,6 +1,7 @@
 import config from ".";
 import { Pool } from "pg";
 import PersonController from "../controllers/PersonController";
+import { logger } from "./logger";
 
 const pool = new Pool(config.db);
 const personController = new PersonController();
@@ -8,10 +9,10 @@ const personController = new PersonController();
 const checkConnection = async () => {
   try {
     const res = await pool.query("SELECT NOW()");
-    console.log("[Database] Connection successful:", res.rows);
+    logger.info("[Database] Connection successful:", res.rows);
     personController.initializePersonDb();
   } catch (error) {
-    console.error("[Database] Connection error:", error);
+    logger.error("[Database] Connection error:", error);
   } finally {
     await pool.end();
   }
